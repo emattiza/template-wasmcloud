9f4a1c9262dd8ac4a7673d25206d5508984d3a56
Evan Mattiza <emattiza@gmail.com>
2022-03-14 15:15:19 +0000

    fixed for proper DASHBOARD_HOST

--- wasm-cloud-base/command.sh
+++ wasm-cloud-base/command.sh
@@ -1,3 +1,3 @@
 export GP_HTTPS_HOST=$(gp url 4000)
-export GP_HOST=echo "$GP_HTTPS_HOST" | sed s#https://##
+export GP_HOST=$(echo "$GP_HTTPS_HOST" | sed s#https://##)
 docker-compose -f wasm-cloud-base/docker-compose.yml up
